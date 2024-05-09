b7a98cf7d75cb40567ae13629b3cc9cab1c3b149
David Basantes <sebastian2405lucero@hotmail.com>
2024-05-09 15:44:13 -0500

    Exclude backend

--- remote_build.sh
+++ remote_build.sh
@@ -4,7 +4,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://api.linkbio.david-dev.dev reflex export --frontend-only
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
