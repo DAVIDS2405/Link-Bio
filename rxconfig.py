6de7efc27057c6136d394b44e032262927036a1f
David Basantes <sebastian2405lucero@hotmail.com>
2024-05-09 16:33:17 -0500

    Exclude backend

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    cors_allowed_origins=[
-        'https://linkbio.david-dev.dev',
-        'http://localhost:3000'
-    ]
+    # cors_allowed_origins=[
+    #     'https://linkbio.david-dev.dev',
+    #     'http://localhost:3000'
+    # ]
 )
