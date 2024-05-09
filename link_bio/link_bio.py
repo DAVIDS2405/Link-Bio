b9e98dcd23875d91729bdc4f212a15b3c39cda86
David Basantes <sebastian2405lucero@hotmail.com>
2024-05-09 16:50:41 -0500

    Exclude backend

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -5,8 +5,3 @@ from .pages.index import index
 app = rx.App(style=styles.BASE_STYLE, stylesheets=['fonts/font.css'])
 
 app.add_page(index, route="/")
-
-
-# @app.api.get("/", include_in_schema=False)
-# async def redirect_doc():
-#     return RedirectResponse("/docs")
--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    # cors_allowed_origins=[
-    #     'https://linkbio.david-dev.dev',
-    #     'http://localhost:3000'
-    # ]
+
 )
