0ae01ba85097428a0a1c5638999c2aadcb7d7db9
David Basantes <sebastian2405lucero@hotmail.com>
2024-05-09 16:55:50 -0500

    Exclude backend

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -1,6 +1,5 @@
 import reflex as rx
 import link_bio.styles.Styles as styles
-from fastapi.responses import RedirectResponse
 from .pages.index import index
 app = rx.App(style=styles.BASE_STYLE, stylesheets=['fonts/font.css'])
 
